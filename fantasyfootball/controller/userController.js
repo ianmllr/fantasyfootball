88b3962b0cb0fddcb906ed678b9e972d3456eb44
ianmllr <130236399+ianmllr@users.noreply.github.com>
2025-11-17 13:14:12 +0100

    create user, installeret bcrypt

---
+++ fantasyfootball/controller/userController.js
@@ -0,0 +1,35 @@
+const User = require('../models/userModel');
+
+// CREATE user
+exports.createUser = async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
+        if (!username) {
+            return res.status(400).json({
+                message: "Username is required."
+            })
+        }
+        if (!password) {
+            return res.status(400).json({
+                message: "Password is required."
+            })
+        }
+        if (!email) {
+            return res.status(400).json({
+                message: "Email is required."
+            })
+        }
+        const user = await User.create({ username, password, email });
+
+        const userResponse = user.toObject();
+        delete userResponse.password;
+        res.status(201).json({
+            data: userResponse
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+            }
+        )
+    }
+}
